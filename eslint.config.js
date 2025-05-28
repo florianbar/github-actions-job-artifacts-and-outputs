3536e86e42c420d2133f6f0ca9c536b90da2e8a8
Florian Bär <florianbar01@gmail.com>
2025-05-28 10:02:34 +0200

    fix es lint

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    rules: js.configs.recommended.rules,
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{jsx,js}"],
+    plugins: { react: pluginReact },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "18.2.0",
+      },
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Turn off requirement to import React
+    },
+  },
+  {
+    files: ["**/*.test.{js,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+      },
+    },
+  },
+];
